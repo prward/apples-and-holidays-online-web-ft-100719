1c6658f1737409d5de452cc936d5d0f74a901e44
Patrick Ward <patrick.r.ward@verizon.net>
2019-10-11 15:31:29 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -71,9 +71,10 @@ def all_holidays_with_bbq(holiday_hash)
 holiday_hash.map do |season, holiday|
     holiday.map do |holiday, item|
       holiday if item.include?("BBQ")
+    end
   end.flatten.compact
 end
-end
+
 
 
 
